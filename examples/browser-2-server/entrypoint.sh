d0011c4c6780a3703758686c58a509c080350a18
Elliot Braem <elliot@ejlbraem.com>
2025-08-26 17:00:59 -0500

    add port

--- examples/browser-2-server/entrypoint.sh
+++ examples/browser-2-server/entrypoint.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 # strip https:// or http:// from domain if necessary
 export APP_HOST=${APP_DOMAIN##*://}
 
-echo "Using API: ${API_DOMAIN}"
+echo "Using API: ${API_DOMAIN} with port: ${API_PORT}"
 echo "Using APP: ${APP_DOMAIN}"
 
 exec caddy run --config Caddyfile --adapter caddyfile 2>&1
