177998df4c527a77a05367175b7557a8465ab1ed
Elliot Braem <elliot@ejlbraem.com>
2025-10-29 15:10:10 -0500

    dev tools

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,9 +1,8 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { orpc, queryClient } from "@/utils/orpc";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { TanStackDevtools } from '@tanstack/react-devtools'
 import {
   HeadContent,
   Outlet,
@@ -71,8 +70,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
           </QueryClientProvider>
           <Toaster richColors />
         </ThemeProvider>
-        <TanStackRouterDevtools position="bottom-right" />
-        <ReactQueryDevtools buttonPosition="bottom-left" />
+        <TanStackDevtools />
         <Scripts />
       </body>
     </html>
